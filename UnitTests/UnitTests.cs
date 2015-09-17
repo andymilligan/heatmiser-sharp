ee0c0c66c6f86d6a65759e97a6d9540a111e1b30
Andy Milligan <andymilligan@hotmail.com>
2015-09-18 00:03:21 +0100

    Added test

---
+++ UnitTests/UnitTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using heatmiser_sharp;
+
+namespace heatmiser_sharp.UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void GetState()
+        {
+            heatmiser_sharp.State state = new State();
+
+            Assert.IsNotNull(state.TempActual);
+            Assert.IsNotNull(state.TempSetting);
+        }
+    }
+}
